52db5009ce41b8fd456c6c422d1b3e93002e5e6a
Aanand-Patil <anandpatil6440@gmail.com>
2025-01-09 23:41:16 +0530

    hovercard component sucesfully used

--- src/app/resister/page.tsx
+++ src/app/resister/page.tsx
@@ -1,3 +1,4 @@
+import HoverCardDemo from "@/components/hovercard/HoverC";
 import Link from "next/link";
 import React from "react";
 
@@ -6,13 +7,7 @@ type Props = {};
 function Resister({}: Props) {
   return (
     <section className="mt-7">
-      <h1 className="text-center text-4xl text-red-500">resister</h1>
-      <form className="block max-w-sm mx-auto">
-        <input type="email" placeholder="email" />
-
-        <input type="password" placeholder="password" />
-        <button type="submit">resister</button>
-      </form>
+      <HoverCardDemo />
     </section>
   );
 }
